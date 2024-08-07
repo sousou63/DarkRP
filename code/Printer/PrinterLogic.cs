6548134d6f3d8db0e6327fcc60b7e0d8c220f634
sousou63 <62064352+sousou63@users.noreply.github.com>
2024-08-07 11:14:17 +0200

    Printer Logic encapsulation

---
+++ code/Printer/PrinterLogic.cs
@@ -0,0 +1,14 @@
+using Sandbox;
+
+public sealed class PrinterLogic : Component
+{
+
+	// Base Printer Propertys
+
+	[Property] public Color Bronze { get; set; }
+	[Property] public Color Silver { get; set; }
+	[Property] public Color Gold { get; set; }
+	[Property] public Color Diamond { get; set; }
+
+
+}
