5bfd85053f5b84667cff795b29842e6c6162e49c
sousou63 <62064352+sousou63@users.noreply.github.com>
2024-08-11 14:47:57 +0200

    fix: game controller reference logic (by QueenPM) + audio error sound path fix ( by Sousou )

--- code/Config/CommandConfig.cs
+++ code/Config/CommandConfig.cs
@@ -2,156 +2,190 @@ using System;
 
 namespace Commands
 {
-  /// <summary>
-  /// Interface for command configuration.
-  /// </summary>
-  public interface ICommandConfig
-  {
-    string Name { get; }
-    string Description { get; }
-    int PermissionLevel { get; }
-    // TODO the command function should also take in the GameObject player parameter to be able to send messages to the player perhaps.
-    bool CommandFunction( string[] args );
-  }
+	/// <summary>
+	/// Interface for command configuration.
+	/// </summary>
+	public interface ICommandConfig
+	{
+		string Name { get; }
+		string Description { get; }
+		int PermissionLevel { get; }
+		// TODO the command function should also take in the GameObject player parameter to be able to send messages to the player perhaps.
+		bool CommandFunction( string[] args );
+	}
 
-  /// <summary>
-  /// Represents a command with a name, description, permission level, and a function to execute.
-  /// </summary>
-  public class Command : ICommandConfig
-  {
-      /// <summary>
-      /// Gets the name of the command.
-      /// </summary>
-      public string Name { get; }
-  
-      /// <summary>
-      /// Gets the description of the command.
-      /// </summary>
-      public string Description { get; }
-  
-      /// <summary>
-      /// Gets the permission level required to execute the command. Currently does nothing.
-      /// </summary>
-      public int PermissionLevel { get; } = 0;
-  
-      /// <summary>
-      ///  The function to execute when the command is called.
-      /// </summary>
-      private readonly Func<string[], bool> commandFunction;
-  
-      /// <summary>
-      /// Initializes a new instance of the Command class.
-      /// </summary>
-      /// <param name="name">The name of the command.</param>
-      /// <param name="description">The description of the command.</param>
-      /// <param name="permissionLevel">The permission level required to execute the command.</param>
-      /// <param name="commandFunction">The function to execute when the command is called.</param>
-      /// <exception cref="ArgumentNullException">
-      /// Thrown when <paramref name="name"/>, <paramref name="description"/>, or <paramref name="commandFunction"/> is null.
-      /// </exception>
-      public Command(string name, string description, int permissionLevel, Func<string[], bool> commandFunction)
-      {
-          Name = name.ToLowerInvariant() ?? throw new ArgumentNullException(nameof(name));
-          Description = description ?? throw new ArgumentNullException(nameof(description));
-          PermissionLevel = permissionLevel;
-          this.commandFunction = commandFunction ?? throw new ArgumentNullException( nameof( commandFunction ) );
-      }
-  
-      /// <summary>
-      /// Executes the command function with the provided arguments.
-      /// </summary>
-      /// <param name="args">The arguments to pass to the command function. Can be null.</param>
-      /// <returns>True if the command executed successfully; otherwise, false.</returns>
-      public bool CommandFunction(string[] args = null) => commandFunction(args);
-  }
+	/// <summary>
+	/// Represents a command with a name, description, permission level, and a function to execute.
+	/// </summary>
+	public class Command : ICommandConfig
+	{
+		/// <summary>
+		/// Gets the name of the command.
+		/// </summary>
+		public string Name { get; }
 
-  /// <summary>
-  /// Command configuration.
-  /// </summary>
-  public class CommandConfig
-  {
-    private readonly Dictionary<string, ICommandConfig> _commands = new();
+		/// <summary>
+		/// Gets the description of the command.
+		/// </summary>
+		public string Description { get; }
 
-    public IReadOnlyCollection<ICommandConfig> Commands => _commands.Values;
-    /// <summary>
-    /// Registers a command.
-    /// </summary>
-    /// <param name="command"></param>
-    /// <exception cref="ArgumentNullException"></exception>
-    /// <exception cref="InvalidOperationException"></exception>
-    public void RegisterCommand( ICommandConfig command )
-    {
-      if ( command == null )
-        throw new ArgumentNullException( nameof( command ) );
+		/// <summary>
+		/// Gets the permission level required to execute the command. Currently does nothing.
+		/// </summary>
+		public int PermissionLevel { get; } = 0;
 
-      var commandNameLower = command.Name.ToLowerInvariant();
-      if ( _commands.ContainsKey( commandNameLower ) )
-        throw new InvalidOperationException( $"Command with name \"{commandNameLower}\" already exists." );
+		/// <summary>
+		///  The function to execute when the command is called.
+		/// </summary>
+		private readonly Func<string[], bool> commandFunction;
 
-      _commands[commandNameLower] = command;
-    }
+		/// <summary>
+		/// Initializes a new instance of the Command class.
+		/// </summary>
+		/// <param name="name">The name of the command.</param>
+		/// <param name="description">The description of the command.</param>
+		/// <param name="permissionLevel">The permission level required to execute the command.</param>
+		/// <param name="commandFunction">The function to execute when the command is called.</param>
+		/// <exception cref="ArgumentNullException">
+		/// Thrown when <paramref name="name"/>, <paramref name="description"/>, or <paramref name="commandFunction"/> is null.
+		/// </exception>
+		public Command( string name, string description, int permissionLevel, Func<string[], bool> commandFunction )
+		{
+			Name = name.ToLowerInvariant() ?? throw new ArgumentNullException( nameof( name ) );
+			Description = description ?? throw new ArgumentNullException( nameof( description ) );
+			PermissionLevel = permissionLevel;
+			this.commandFunction = commandFunction ?? throw new ArgumentNullException( nameof( commandFunction ) );
+		}
 
-    public void UnregisterCommand( string commandName )
-    {
-      commandName = commandName.ToLowerInvariant();
-      if ( string.IsNullOrWhiteSpace( commandName ) )
-        throw new ArgumentException( "Command name cannot be null or whitespace.", nameof( commandName ) );
+		/// <summary>
+		/// Executes the command function with the provided arguments.
+		/// </summary>
+		/// <param name="args">The arguments to pass to the command function. Can be null.</param>
+		/// <returns>True if the command executed successfully; otherwise, false.</returns>
+		public bool CommandFunction( string[] args = null ) => commandFunction( args );
+	}
 
-      if ( !_commands.Remove( commandName ) )
-        throw new KeyNotFoundException( $"Command with name \"{commandName}\" does not exist." );
-    }
+	/// <summary>
+	/// Command configuration.
+	/// </summary>
+	public class CommandConfig
+	{
+		private readonly Dictionary<string, ICommandConfig> _commands = new();
 
-    public ICommandConfig GetCommand( string commandName )
-    {
-      // Lowercase the command name to make it case-insensitive.
-      commandName = commandName.ToLowerInvariant();
-      if ( string.IsNullOrWhiteSpace( commandName ) )
-        throw new ArgumentException( "Command name cannot be null or whitespace.", nameof( commandName ) );
+		public IReadOnlyCollection<ICommandConfig> Commands => _commands.Values;
+		/// <summary>
+		/// Registers a command.
+		/// </summary>
+		/// <param name="command"></param>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="InvalidOperationException"></exception>
+		public void RegisterCommand( ICommandConfig command )
+		{
+			if ( command == null )
+				throw new ArgumentNullException( nameof( command ) );
 
-      if ( _commands.TryGetValue( commandName, out var command ) )
-        return command;
+			var commandNameLower = command.Name.ToLowerInvariant();
+			if ( _commands.ContainsKey( commandNameLower ) )
+				throw new InvalidOperationException( $"Command with name \"{commandNameLower}\" already exists." );
 
-      throw new KeyNotFoundException( $"Command with name {commandName} does not exist." );
-    }
+			_commands[commandNameLower] = command;
+		}
 
-    public string[] GetCommandNames() => _commands.Keys.ToArray();
+		public void UnregisterCommand( string commandName )
+		{
+			commandName = commandName.ToLowerInvariant();
+			if ( string.IsNullOrWhiteSpace( commandName ) )
+				throw new ArgumentException( "Command name cannot be null or whitespace.", nameof( commandName ) );
 
-    public bool ExecuteCommand( string commandName, string[] args )
-    {
-      try
-      {
-        var command = GetCommand( commandName );
-        Log.Info( $"Executing command \"{commandName}\"." );
-        return command.CommandFunction( args );
-      }
-      catch ( Exception e )
-      {
-        Log.Error( $"Failed to execute command \"{commandName}\": {e.Message}" );
-        return false;
-      }
-    }
-  }
+			if ( !_commands.Remove( commandName ) )
+				throw new KeyNotFoundException( $"Command with name \"{commandName}\" does not exist." );
+		}
+
+		public ICommandConfig GetCommand( string commandName )
+		{
+			// Lowercase the command name to make it case-insensitive.
+			commandName = commandName.ToLowerInvariant();
+			if ( string.IsNullOrWhiteSpace( commandName ) )
+				throw new ArgumentException( "Command name cannot be null or whitespace.", nameof( commandName ) );
+
+			if ( _commands.TryGetValue( commandName, out var command ) )
+				return command;
+
+			throw new KeyNotFoundException( $"Command with name {commandName} does not exist." );
+		}
+
+		public string[] GetCommandNames() => _commands.Keys.ToArray();
+
+		public bool ExecuteCommand( string commandName, string[] args )
+		{
+			try
+			{
+				var command = GetCommand( commandName );
+				Log.Info( $"Executing command \"{commandName}\"." );
+				return command.CommandFunction( args );
+			}
+			catch ( Exception e )
+			{
+				Log.Error( $"Failed to execute command \"{commandName}\": {e.Message}" );
+				return false;
+			}
+		}
+	}
 }
 
 public static class ConfigManagerHelper
 {
-  private static readonly Guid ConfigManagerGuid = new Guid( "7c7e467f-11f7-43c7-accf-abc02a3790ea" );
+	public static GameObject GameController { get; set; } = null;
+	public static ConfigManager ConfigManager { get; set; } = null;
 
-  public static ConfigManager GetConfigManager( Scene scene )
-  {
-    try
-    {
-      var configManager = scene.Directory.FindComponentByGuid( ConfigManagerGuid ) as ConfigManager;
-      if ( configManager is null )
-      {
-        Log.Error( "Config Manager not found" );
-      }
-      return configManager;
-    }
-    catch ( Exception e )
-    {
-      Log.Error( $"Failed to get Config Manager: {e.Message}" );
-      return null;
-    }
-  }
+	private static GameObject FetchCacheGameController( Scene scene )
+	{
+		try
+		{
+			if ( GameController != null ) return GameController;
+			GameController = scene.Directory.FindByName( "Game Controller" )?.First();
+			if ( GameController == null )
+			{
+				Log.Error( "Game Controller not found" );
+			}
+			return GameController;
+		}
+		catch ( Exception e )
+		{
+			Log.Error( $"Failed to fetch game controller: {e.Message}" );
+			return null; // Ensure a value is returned
+		}
+	}
+
+	private static ConfigManager FetchCacheConfigManager( Scene scene )
+	{
+		try
+		{
+			if ( ConfigManager != null ) return ConfigManager;
+			ConfigManager = FetchCacheGameController( scene )?.Components.Get<ConfigManager>();
+			if ( ConfigManager == null )
+			{
+				Log.Error( "Config Manager not found" );
+			}
+			return ConfigManager;
+		}
+		catch ( Exception e )
+		{
+			Log.Error( $"Failed to fetch Config Manager: {e.Message}" );
+			return null; // Ensure a value is returned
+		}
+	}
+
+	public static ConfigManager GetConfigManager( Scene scene )
+	{
+		try
+		{
+			return FetchCacheConfigManager( scene );
+		}
+		catch ( Exception e )
+		{
+			Log.Error( $"Failed to get Config Manager: {e.Message}" );
+			return null; // Ensure a value is returned
+		}
+	}
 }
