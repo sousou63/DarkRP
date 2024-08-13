1b9eb57e9425947ff86a595682e9adf607c602ba
sousou63 <62064352+sousou63@users.noreply.github.com>
2024-08-14 01:24:13 +0200

    fixed

--- code/Extensions/ConnectionExtensions.cs
+++ code/Extensions/ConnectionExtensions.cs
@@ -4,7 +4,7 @@ public static class ConnectionExtensions
 	[ConVar( "dev_admin" )]
 	public static bool DevsAreAdmins { get; set; } = true;
 
-	public const ulong DEV_STEAM_ID = ;
+	public const ulong DEV_STEAM_ID = 0;
 
 	public static bool IsAdmin( this Connection conn )
 		=> conn?.IsHost == true || ( DevsAreAdmins && IsDev( conn ) );
