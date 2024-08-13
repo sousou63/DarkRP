b53dc7a12cf873133c31065ed7f24e17aec46d6f
QueenPM <danimark644@gmail.com>
2024-08-13 13:31:01 +0100

    feat: added player namespace and public class

---
+++ code/Player/Player.cs
@@ -0,0 +1,13 @@
+namespace PlayerDetails {
+  	public class Player
+    {
+      public GameObject GameObject { get; set; }
+      public Connection Connection { get; set; }
+
+      public Player( GameObject gameObject, Connection connection )
+      {
+        GameObject = gameObject;
+        Connection = connection;
+      }
+    }
+}
